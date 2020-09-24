b81566c34670c52a706184b87689b8be5d3571ff
maksim.voikov <maksim.voikov@bookinggroup.com>
2020-09-24 16:57:09 +0300

    fixing addresses

--- nginx-uninstall/vars.sh
+++ nginx-uninstall/vars.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-vhost1_Name="vhost-1.airbaltic.com"
-vhost2_Name="vhost-2.airbaltic.com"
+vhost1_Name="vhost-1.airb.com"
+vhost2_Name="vhost-2.airb.com"
 index1_Text="=======Welcome!======="
